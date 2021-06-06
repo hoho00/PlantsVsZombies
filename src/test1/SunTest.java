12853e7609b9138915f1b37396ef7b0a583c24e9
201524547 <qht6@naver.com>
2021-06-06 17:28:26 +0900

    Add Test File

---
+++ src/test1/SunTest.java
@@ -0,0 +1,15 @@
+package test1;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class SunTest {
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){}
+
+}
